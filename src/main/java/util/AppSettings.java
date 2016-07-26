616348995d41dec5cdbc5a29681145189c089254
slonikmak <slonikmak@gmail.com>
2016-07-26 22:46:57 +0300

    update AppSettings class

--- src/main/java/util/AppSettings.java
+++ src/main/java/util/AppSettings.java
@@ -5,15 +5,21 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
-/**
+/**Класс для работы с настройками приложения
+ * включает:
+ * -папка синхронизации SINCHRONIZATION_PATH
+ * -..
  * @author Cloudraid Dev Team (cloudraid.stnetix.com)
  */
 
 public class AppSettings {
     private static final String PROPERTIES_FILE_NAME = "main.ini";
     public static Properties properties = new Properties();
+    public static Map<String, String> prop = new HashMap<>();
 
     public static void loadProperties(){
         try {
@@ -21,10 +27,18 @@ public class AppSettings {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        prop.put("SINCHRONIZATION_PATH", properties.getProperty("SINCHRONIZATION_PATH"));
     }
     public static void saveProperties(){
-
+        /**
+         * TODO реализовать сохранение настроек. если не вызывать метод, настройки не сохраняются
+         */
     }
+    public static void setProperty(String key, String val){
+        prop.put(key,val);
+    }
+
+
 
 
 }
